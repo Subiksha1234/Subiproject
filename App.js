c2a737057676dda94fe0e31c04273e6e99921782
Subiksha1234 <120357012+Subiksha1234@users.noreply.github.com>
2022-12-12 11:09:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Search from './components/Search/Search';
+import { Home } from './components/Home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+       <Search />
+    </div>
+  );
+}
+
+export default App;
